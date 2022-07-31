66ea696fd8df9418c83b090f735916e5525f6995
Владислав Антонов <lolik1806@gmail.com>
2022-07-31 10:26:23 +0300

    swap rows and columns

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 const size = 9
@@ -41,6 +43,66 @@ func generateSudoku() sudoku {
 	}
 
 	return s
+}
+
+func (s *sudoku) transposing() {
+	for i := 0; i < size; i++ {
+		for j := i; j < size; j++ {
+			if i != j {
+				s[i][j], s[j][i] = s[j][i], s[i][j]
+			}
+		}
+	}
+}
+
+func (s *sudoku) getRandomArea() int {
+	rand.Seed(time.Now().UnixNano())
+
+	var area int
+
+	switch rand.Intn(2) {
+	case 0:
+		area = 0
+	case 1:
+		area = 3
+	default:
+		area = 6
+	}
+
+	return area
+}
+
+func (s *sudoku) swapRowsSmall() {
+	area := s.getRandomArea()
+
+	row1, row2 := rand.Intn(3)+area, rand.Intn(3)+area
+
+	for i := 0; i < size; i++ {
+		s[row1][i], s[row2][i] = s[row2][i], s[row1][i]
+	}
+}
+
+func (s *sudoku) swapColumnsSmall() {
+	area := s.getRandomArea()
+
+	col1, col2 := rand.Intn(3)+area, rand.Intn(3)+area
+
+	for i := 0; i < size; i++ {
+		s[i][col1], s[i][col2] = s[i][col2], s[i][col1]
+	}
+}
+
+func (s *sudoku) swapRowsArea() {
+	area1, area2 := s.getRandomArea(), s.getRandomArea()
+
+	if area1 == area2 {
+		return
+	}
+
+}
+
+func (s *sudoku) swapColumnsArea() {
+
 }
 
 func (s *sudoku) set(row, col, dig int8) error {
@@ -83,6 +145,11 @@ func (s *sudoku) print() {
 
 func main() {
 	newSudoku := generateSudoku()
+
+	newSudoku.print()
+
+	//newSudoku.transposing()
+	newSudoku.swapRowsSmall()
 
 	newSudoku.print()
 }
